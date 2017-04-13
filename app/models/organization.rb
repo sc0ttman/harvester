d64e5bee1850d6a10de7d28aa8b60050b50d39d8
sc0ttman <scott1979+git@gmail.com>
2017-04-13 17:31:14 -0600

    Show more info about the orgs on config page

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,4 +3,8 @@ class Organization < ApplicationRecord
   has_many :users
   has_many :projects
   has_many :tasks
+
+  def configured_project
+    projects.where(id: harvest_project).first
+  end
 end
